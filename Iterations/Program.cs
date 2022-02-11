7f4d1e99d007f67bdf4e0a92ea9bbdbeb1268d81
macuser301 <macuser30@yahoo.com>
2022-02-11 10:01:08 -0600

    init commit

---
+++ Iterations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                Console.WriteLine($"{i}");
+                
+
+            }
+            Console.WriteLine("Loops 15 times");
+        
+        
+        }
+    }
+}
